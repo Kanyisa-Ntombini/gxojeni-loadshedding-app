f09b799c3bd3505a15e43dfd0d60eb011caaf0a3
Kanyisa-Ntombini <kanyisa.ntombini@gmail.com>
2023-03-19 00:05:59 +0200

    Cleaned up code

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
-# gxojeni
+# Gxojeni Weather Project
+
+I will be using Open Weather Map to get weather focus for the next five days.
+
+## Call 5 day / 3 hour forecast data
+- You can search weather forecast for 5 days with data every 3 hours by geographic coordinates.
+- api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}
+
+## Important links
+
+### Weather triggers
+- Used to create triggers which will fire on an occurrence of the selected weather conditions (temperature, humidity, pressure, etc.)
+- https://openweathermap.org/triggers
+
+### Air Pollution API
+- https://openweathermap.org/api/air-pollution
+
+### API for Coordinates - Geocoding API
+- https://openweathermap.org/api/geocoding-api
--- src/getWeather.js
+++ src/getWeather.js
@@ -1,28 +1,82 @@
 const axios = require('axios');
+const { kelvinToCelcius } = require('./helper_functions');
+require('dotenv').config();
+const { API_KEY } = process.env;
 
-const apiKey = '41ac969b72bf4e060230ca40a68ef22a';
-const lat = null;
-const lon = null;
-const country = 'za';
-const state = 'gauteng';
+async function getWeatherData(town, state, countryCode, limit) {
+  let lat = null;
+  let lon = null;
+  let townCoordinates = null;
+  let weatherResponse = null;
 
+  try {
+    townCoordinates = await axios.get(
+      `http://api.openweathermap.org/geo/1.0/direct?q=${town}&limit=5&appid=${API_KEY}`
+    );
+  } catch (error) {
+    console.log(error);
+  }
 
-async function getWeatherData(town, limit) {
-   const coordinatesURL = `http://api.openweathermap.org/geo/1.0/direct?q=${town},${state},${country}&limit=${limit}&appid=${apiKey}`;
-   const getTownCoordinates = await axios.get(coordinatesURL);
+  if (
+    townCoordinates.status === 200 &&
+    townCoordinates.data[0].name.toLowerCase() == town &&
+    townCoordinates.data[0].country.toLowerCase() === countryCode &&
+    townCoordinates.data[0].state.toLowerCase() === state
+  ) {
+    lat = townCoordinates.data[0].lat;
+    lon = townCoordinates.data[0].lon;
+  }
 
-   if (getTownCoordinates.status === 200) {
-      if (getTownCoordinates.data[0].name === town && getTownCoordinates.data[0].country === country && getTownCoordinates.data[0].state === state) {
-         lat = getTownCoordinates.data[0].lat;
-         lon = getTownCoordinates.data[0].lon
-      }
-   }
+  try {
+    weatherResponse = await axios.get(
+      `http://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${API_KEY}`
+    );
+  } catch (error) {
+    console.log(error);
+  }
 
-   const weatherURL = `http://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${apiKey}`;
+  const mainTemp = kelvinToCelcius(weatherResponse.data.list[0].main.temp);
+  const feelsLikeTemp = kelvinToCelcius(
+    weatherResponse.data.list[0].main.feels_like
+  );
+  const minTemp = kelvinToCelcius(weatherResponse.data.list[0].main.temp_min);
+  const maxTemp = kelvinToCelcius(weatherResponse.data.list[0].main.temp_max);
+  const pressure = weatherResponse.data.list[0].main.grnd_level;
+  const humidity = weatherResponse.data.list[0].main.humidity;
 
-   const response = await axios.get(weatherURL);
+  const clouds = weatherResponse.data.list[0].clouds.all;
+  const windSpeed = weatherResponse.data.list[0].wind.speed;
+  const windDirection = weatherResponse.data.list[0].wind.deg;
+  const visibility = weatherResponse.data.list[0].visibility;
+  const probabilityRain = weatherResponse.data.list[0].pop;
+  //   const rainVolLast3Hours = kelvinToCelcius(weatherResponse.data.list[0].rain.3h);
 
-   console.log(response.data);
+  //   console.log(weatherResponse.data);
+  //   console.log(`mainTemp = ${mainTemp}`);
+  //   console.log(`feelsLikeTemp = ${feelsLikeTemp}`);
+  //   console.log(`minTemp = ${minTemp}`);
+  //   console.log(`maxTemp = ${maxTemp}`);
+  //   console.log(`pressure = ${pressure}`);
+  //   console.log(`humidity = ${humidity}`);
+  // console.log(`clouds = ${clouds}`);
+  //   console.log(`windspeed = ${windSpeed}`);
+  //   console.log(`wind direction = ${windDirection}`);
+  //   console.log(`visibility = ${visibility}`);
+  //   console.log(`probability rain = ${probabilityRain}`);
+
+  return {
+    mainTemp,
+    feelsLikeTemp,
+    minTemp,
+    maxTemp,
+    pressure,
+    humidity,
+    clouds,
+    windSpeed,
+    windDirection,
+    visibility,
+    probabilityRain,
+  };
 }
 
-getWeatherData('midrand', 5);
+getWeatherData('midrand', 'gauteng', 'za', 5);
