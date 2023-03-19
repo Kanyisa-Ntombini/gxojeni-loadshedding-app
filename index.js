751a9ddaace81402b37fc0a80682c246b72b70b1
Kanyisa-Ntombini <kanyisa.ntombini@gmail.com>
2023-03-19 12:07:33 +0200

    random changes

--- index.js
+++ index.js
@@ -1,18 +1,21 @@
 import React from 'react';
 import { ReactDOM } from 'react';
+// const {weatherData} = require('./something);
+
+//update DOM with weather data
 
 // const React = require('react');
 // const ReactDOM = require('react-dom');
 
-ReactDOM.render(<h1>Render me!</h1>, document.getElementById('app'));
-console.log('hello')
+// ReactDOM.render(<h1>Render me!</h1>, document.getElementById('app'));
+// console.log('hello')
 
-const cottageLife = (
-   <ul>
-      <li>flowers</li>
-      <li>herbs</li>
-      <li>trees</li>
-   </ul>
-);
+// const cottageLife = (
+//    <ul>
+//       <li>flowers</li>
+//       <li>herbs</li>
+//       <li>trees</li>
+//    </ul>
+// );
 
-ReactDOM.render(cottageLife, document.getElementById('rural'));
+// ReactDOM.render(cottageLife, document.getElementById('rural'));
